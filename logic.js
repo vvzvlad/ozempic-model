94e355788933be792c1f9fba8c82f26bcc23d860
vvzvlad <git@vvzvlad.xyz>
2024-11-06 01:57:26 +0300

    feat: Add satiety tracking functionality to dosing schedule logic

--- logic.js
+++ logic.js
@@ -1,10 +1,10 @@
 var chart = null;
-var default_json_data = '{ "thresholds":{"from":13,"to":15}, "pharmacokinetics":{"tmax":2,"elimination_halflife":7},  "dosing_schedule":[["2024-08-30","0.25"],["2024-09-01","0.25"],["2024-09-08","0.25"],["2024-09-15","0.25"],["2024-09-22","0.25"],["2024-09-25","0.25"],["2024-09-29","0.25"],["2024-10-05","0.5"],["2024-10-13","0.5"],["2024-10-20","0.25"],["2024-10-24","0.25"],["2024-10-26","0.25"],["2024-10-30","0.25"],["2024-11-02","0.25"],["2024-11-05","0.25"],["2024-11-09","0.25"],["2024-11-12","0.25"],["2024-11-15","0.25"],["2024-11-19","0.25"],["2024-11-22","0.25"],["2024-11-26","0.25"],["2024-11-29","0.25"] ]}'
+var default_json_data = '{ "thresholds":{"from":13,"to":15}, "pharmacokinetics":{"tmax":2,"elimination_halflife":7}, "satiety":[["2024-08-30","4"], ["2024-08-28","4"]],  "dosing_schedule":[["2024-08-30","0.25"],["2024-09-01","0.25"],["2024-09-08","0.25"],["2024-09-15","0.25"],["2024-09-22","0.25"],["2024-09-25","0.25"],["2024-09-29","0.25"],["2024-10-05","0.5"],["2024-10-13","0.5"],["2024-10-20","0.25"],["2024-10-24","0.25"],["2024-10-26","0.25"],["2024-10-30","0.25"],["2024-11-02","0.25"],["2024-11-05","0.25"],["2024-11-09","0.25"],["2024-11-12","0.25"],["2024-11-15","0.25"],["2024-11-19","0.25"],["2024-11-22","0.25"],["2024-11-26","0.25"],["2024-11-29","0.25"] ]}'
 var dose_options = [0, 0.25, 0.5, 1, 2];
 
 var mg_nmmol_ratio = 16 / 0.8; // Conversion ratio from mg to nmol/L
 
-function add_row() {
+function add_row_dosing() {
     var tbody = document.querySelector("#dosing-schedule-table tbody");
     var new_row = document.createElement("tr");
 
@@ -40,13 +40,48 @@ function add_row() {
     save_data_render_chart();
 }
 
-function remove_row() {
+function remove_row_dosing() {
     var tbody = document.querySelector("#dosing-schedule-table tbody");
     if (tbody.children.length > 0) {
         tbody.removeChild(tbody.lastChild);
         save_data_render_chart();
     }
 }
+
+function add_row_satiety() {
+    var tbody = document.querySelector("#satiety-table tbody");
+    var new_row = document.createElement("tr");
+
+    var date_cell = document.createElement("td");
+    var date_input = document.createElement("input");
+    date_input.type = "date";
+    date_input.classList.add("date-input");
+    var last_row_date_input = tbody.lastChild.querySelector(".date-input");
+    var new_date = new Date(last_row_date_input.value);
+    new_date.setDate(new_date.getDate() + 1);
+    date_input.value = new_date.toISOString().split("T")[0];
+    date_cell.appendChild(date_input);
+    new_row.appendChild(date_cell);
+
+    var satiety_cell = document.createElement("td");
+    var satiety_input = document.createElement("input");
+    satiety_input.type = "number";
+    satiety_input.step = 0.5;
+    satiety_input.value = 5;
+    satiety_cell.appendChild(satiety_input);
+    new_row.appendChild(satiety_cell);
+
+    tbody.appendChild(new_row);
+}
+
+function remove_row_satiety() {
+    var tbody = document.querySelector("#satiety-table tbody");
+    if (tbody.children.length > 0) {
+        tbody.removeChild(tbody.lastChild);
+    }
+}
+
+
 
 function calculate_concentration_data(data) {
     var tmax_percent = 0.95; // 95% absorption on tmax time
@@ -143,13 +178,20 @@ function render_chart(data) {
 
     //console.log(data);
 
-    // setup first series (Body concentration)
+    // setup series Body concentration
     var series_body_concentration = chart.line(anychart.data.set(data.body_concentration).mapAs({ x: 0, value: 1 }));
     series_body_concentration.name("Body concentration");
     series_body_concentration.tooltip().enabled(true);
     series_body_concentration.legendItem().iconType("line");
 
-    // setup second series (Dosing schedule)
+    var series_socieity = chart.line(anychart.data.set(data.satiety).mapAs({ x: 0, value: 1 }));
+    series_socieity.name("Satiety");
+    series_socieity.stroke({ color: "#FF5733" });
+    series_socieity.tooltip().enabled(true);
+    series_socieity.legendItem().iconType("line");
+    
+
+    // setup series Dosing schedule
     var adjusted_dosing_schedule_data = data.dosing_schedule
         .map(function (entry) {
             var date = entry[0];
@@ -185,7 +227,7 @@ function render_chart(data) {
     series_dosing_schedule.labels().format(function () { return this.getData('dose').toString(); });
     series_dosing_schedule.legendItem().iconType("circle");
 
-    // setup third series (14-day Moving Average)
+    // setup series 14-day Moving Average
     var series_moving_average = chart.line(anychart.data.set(data.moving_average).mapAs({ x: 0, value: 1 }));
     series_moving_average.name("14-day Moving Average");
     series_moving_average.stroke({ dash: "5 2", thickness: 2, color: "#FF5733" });
@@ -252,6 +294,36 @@ function save_data_to_html(data) {
 
         tbody.appendChild(row);
     });
+
+    satiety = data.satiety
+    var tbody_satiety = document.querySelector("#satiety-table tbody");
+    tbody_satiety.innerHTML = "";
+    satiety.forEach(function (entry) {
+        var row = document.createElement("tr");
+
+        var date_cell = document.createElement("td");
+        var date_input = document.createElement("input");
+        date_input.type = "date";
+        date_input.value = entry[0];
+        date_input.classList.add("date-input");
+        date_cell.appendChild(date_input);
+        row.appendChild(date_cell);
+
+        var satiety_cell = document.createElement("td");
+        var satiety_input = document.createElement("input");
+        satiety_input.type = "number";
+        satiety_input.step = 0.5;
+        satiety_input.value = entry[1];
+        satiety_cell.appendChild(satiety_input);
+        row.appendChild(satiety_cell);
+
+        tbody_satiety.appendChild(row);
+    });
+
+
+
+
+
     if (data.thresholds) {
         document.getElementById("threshold-from").value = data.thresholds.from || 13;
         document.getElementById("threshold-to").value = data.thresholds.to || 15;
@@ -264,18 +336,27 @@ function save_data_to_html(data) {
 
 function get_data_from_html() {
     var dosing_data = [];
-    var rows = document.querySelectorAll("#dosing-schedule-table tbody tr");
-    rows.forEach(function (row, index) {
+    var dosing_rows = document.querySelectorAll("#dosing-schedule-table tbody tr");
+    dosing_rows.forEach(function (row, index) {
         var date = row.querySelector(".date-input").value;
         var dose = row.querySelector(`input[name='dose-${index}']:checked`).value;
         dosing_data.push([date, dose]);
     });
+    var satiety_data = [];
+    var satiety_rows = document.querySelectorAll("#satiety-table tbody tr");
+    satiety_rows.forEach(function (row) {
+        var date = row.querySelector(".date-input").value;
+        var satiety = row.querySelector("input[type='number']").value;
+        satiety_data.push([date, satiety]);
+    });
+
     var threshold_from = parseFloat(document.getElementById("threshold-from").value);
     var threshold_to = parseFloat(document.getElementById("threshold-to").value);
     var tmax = parseFloat(document.getElementById("tmax").value);
     var elimination_halflife = parseFloat(document.getElementById("elimination_halflife").value);
     var json_data = {
         dosing_schedule: dosing_data,
+        satiety: satiety_data,
         thresholds: {
             from: threshold_from,
             to: threshold_to
@@ -329,10 +410,14 @@ anychart.onDocumentReady(function () {
     render_chart(chart_data);
 
     document.getElementById("load-json-button").addEventListener("click", from_json_to_html);
-    document.getElementById("add-row-button").addEventListener("click", add_row);
-    document.getElementById("remove-row-button").addEventListener("click", remove_row);
+    document.getElementById("add-row-button-dosing").addEventListener("click", add_row_dosing);
+    document.getElementById("remove-row-button-dosing").addEventListener("click", remove_row_dosing);
+    document.getElementById("add-row-button-satiety").addEventListener("click", add_row_satiety);
+    document.getElementById("remove-row-button-satiety").addEventListener("click", remove_row_satiety);
 
     document.querySelector("#dosing-schedule-table").addEventListener("input", save_data_render_chart);
+    document.querySelector("#satiety-table").addEventListener("input", save_data_render_chart);
+
     document.getElementById("threshold-from").addEventListener("input", save_data_render_chart);
     document.getElementById("threshold-to").addEventListener("input", save_data_render_chart);
     document.getElementById("tmax").addEventListener("input", save_data_render_chart);
